9f20d3ef26eda3a1c4223aac26ffd3b62c253753
josufmc@hotmail.com <josufmc@hotmail.com>
2017-02-01 13:46:57 +0100

    First JSON test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,37 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    public function pruebasAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('BackendBundle:User')->findAll();
+        return $this->json($users);
+    }
+    
+    public function json($data){
+        $normalizers = array(new GetSetMethodNormalizer());
+        $encoders = array('json' => new JsonEncoder());
+        $serializer = new Serializer($normalizers, $encoders);
+        $json = $serializer->serialize($data, 'json');
+        
+        $response = new Response();
+        $response->setContent($json);
+        $response->headers->set("Content-Type", "application/json");
+        return $response;
     }
 }
