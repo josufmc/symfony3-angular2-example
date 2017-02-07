891a3f9bb12bd1bb35988133e6d4660a6c33beb0
josufmc@hotmail.com <josufmc@hotmail.com>
2017-02-07 07:51:50 +0100

    Added video edit

--- src/AppBundle/Controller/VideoController.php
+++ src/AppBundle/Controller/VideoController.php
@@ -90,4 +90,80 @@ class VideoController extends Controller
         return $helpers->json($data);
     }
     
+    
+    public function editAction(Request $request, $id = null){
+        $helpers = $this->get('app.helpers');
+        $hash = $request->get('authorization', null);
+        $authCheck = $helpers->authCheck($hash);
+        $data = array();
+        if (!$authCheck){
+            $data = array(
+                'status' => 'error',
+                'code' => 401,
+                'msg' => 'Auth error'
+            );
+            return $helpers->json($data);
+        }
+        
+        $identity = $helpers->authCheck($hash, true);
+        $json = $request->get('json', null);
+        if ($json != null){
+            $params = json_decode($json);
+
+            $updatedAt = new \DateTime();
+            $imagen = null;
+            $video_path = null;
+            
+            $user_id = $identity->sub;
+            $title = (isset($params->title))? $params->title: null;
+            $description = (isset($params->description))? $params->description: null;
+            $status = (isset($params->status))? $params->status: null;
+            $em = $this->getDoctrine()->getManager();
+            $video = $em->getRepository('BackendBundle:Video')->findOneBy(array('id'=>$id));
+            
+            if($user_id != null && $title != null){
+                if (isset($identity->sub) && $identity->sub == $video->getUser()->getId()) {
+                    $video->setTitle($title);
+                    $video->setDescription($description);
+                    $video->setStatus($status);
+                    $video->setUpdatedAt($updatedAt);
+
+                    $em->persist($video);
+                    $em->flush();
+
+                    $data = array(
+                            'status' => 'success',
+                            'code' => 201,
+                            'msg' => 'Video update success!!'
+                        );
+                } else {
+                    $data = array(
+                        'status' => 'error',
+                        'code' => 400,
+                        'msg' => 'This user is not the owner!!'
+                    );
+                }
+            } else {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'msg' => 'Video not updated!!'
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'msg' => 'Video not updated!!'
+            );
+        }
+        
+        return $helpers->json($data);
+    }
+    
+    
+    public function uploadAction(Request $request, $id){
+        
+    }
+    
 }
